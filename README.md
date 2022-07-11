87deea9e8ec3b05fb19cbc29be80b5894895f807
Alex Sleek <lux01sv@gmail.com>
2022-07-11 12:20:18 +0300

    EDIT: Litle-bit readme and main file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 💎 TONRECOVER (NOT WORKING CURRENTLY)
-
+Tonrecover (like btcrecover) is utility, to search all seed-phrase for your forgotten ton wallet.
+Currently it works in Python with `ton` library, but we got plan`s to move some perfomance
+modules to Rust. This tool will brute-force all diffirent combinations with your known words.
 ## Installation
 
 Use Python 3.10 and install all requirements using this command:
--- tonrecover.py
+++ tonrecover.py
@@ -1,6 +1,7 @@
 from modules.wordlist import words
 import sys
 
+# TODO: Move this all messages to module
 helpMsg = '''\033[96m _                                               
 | |_ ___  _ __  _ __ ___  ___ _____   _____ _ __ 
 | __/ _ \| '_ \| '__/ _ \/ __/ _ \ \ / / _ \ '__|
@@ -12,9 +13,11 @@ helpMsg = '''\033[96m _
 Usage:
   (Example) main.py blast abadon crater ? true
 '''
+
 def main():
     knownWords = []
     start = False
+    print(len(sys.argv))
     # Display help msg if no arguments
     if len(sys.argv) == 1:
         print(helpMsg)
@@ -23,7 +26,7 @@ def main():
         if arg[0] == '-':
             if arg == '--help' or arg == '-h':
                 print(helpMsg)
-        if arg != "python" and arg != "main.py":
+        if arg != "python" and arg != "tonrecover.py":
             if arg in words:
                 knownWords.append(arg)
             elif arg == "?":
@@ -31,18 +34,18 @@ def main():
             else:
                 print("\033[91mERROR:\033[00m Word \""+arg+"\" is not correct.")
                 break
-    #print(knownWords)
-    if len(knownWords) > 24:
-        print("\033[91mERROR:\033[00m"+'''
-        You entered more than 24 words. do you really need tonrecover?
-        Use -h for help
-        ''')
-    elif len(knownWords) <= 15:
-        print("\u001b[33mWARNING:\033[00m"+'''
-        You entered less than 15 words. Phrase search will take forever
-        ''')
-    if input("Start? (y\\n)") == 'y':
-        start = True
+            #print(knownWords)
+            if len(knownWords) > 24:
+                print("\033[91mERROR:\033[00m"+'''
+                You entered more than 24 words. do you really need tonrecover?
+                Use -h for help
+                ''')
+            elif len(knownWords) <= 15:
+                print("\u001b[33mWARNING:\033[00m"+'''
+                You entered less than 15 words. Seed-phrase search will take forever
+                ''')
+            if input("Start? (y\\n)") == 'y':
+                start = True
 
 if __name__ == "__main__":
     main()
